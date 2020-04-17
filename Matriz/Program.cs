b950f055c368eaf0c07ff0e4d35a0718488812cf
PedroRib76 <p.alessandro@uol.com.br>
2020-04-16 23:50:59 -0300

    Acrescentei a mensagem Terceira Mensagem

--- Matriz/Program.cs
+++ Matriz/Program.cs
@@ -19,6 +19,8 @@ namespace Matriz
             Console.WriteLine("Teste de comando GIT");
 
             Console.WriteLine("Segunda mensagem.");
+
+            Console.WriteLine("Terceira mensagem.");
         }
     }
 }
